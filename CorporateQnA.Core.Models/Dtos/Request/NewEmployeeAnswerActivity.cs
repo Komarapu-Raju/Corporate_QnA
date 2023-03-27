b81309d3c968c8d5dd6e22e554663f7484139f08
Komarapu-Raju <raju.k@keka.com>
2023-03-27 23:41:55 +0530

    added new coremodels

---
+++ CorporateQnA.Core.Models/Dtos/Request/NewEmployeeAnswerActivity.cs
@@ -0,0 +1,11 @@
+﻿namespace CorporateQnA.Core.Models.Dtos.Request
+{
+    public class NewEmployeeAnswerActivity
+    {
+        public int AnswerId { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public int VoteStatus { get; set; }
+    }
+}
