08b3fb8b3e83040b833cb8036523c3dc77682f3a
Komarapu-Raju <raju.k@keka.com>
2023-03-27 14:44:37 +0530

    Dtos were added

---
+++ CorporateQnA.Core.Models/Dtos/AnswerDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorporateQnA.Core.Models.Dtos
+{
+    public class AnswerDto
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime AnsweredOn { get; set; }
+
+        public string AnsweredBy { get; set; }
+
+        public bool IsBestSolution { get; set; }
+
+        public int Upvotes { get; set; }
+
+        public int DownVotes { get; set; }
+    }
+}
---
+++ CorporateQnA.Core.Models/Dtos/QuestionDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorporateQnA.Core.Models.Dtos
+{
+    public class QuestionDto
+    {
+        public int Id { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public int ViewCount { get; set; }
+
+        public int AnswerCount { get; set; }
+
+        public bool IsSolved { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public int Upvotes { get; set; }
+    }
+}
