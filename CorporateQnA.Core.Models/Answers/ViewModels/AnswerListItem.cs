b8d44e3af80b5fe27b2ccb923f8ebac12ddf9a50
Komarapu-Raju <raju.k@keka.com>
2023-03-28 18:26:20 +0530

    sdfghujikop[

--- CorporateQnA.Core.Models/Answers/ViewModels/AnswerListItem.cs
+++ CorporateQnA.Core.Models/Answers/ViewModels/AnswerListItem.cs
@@ -4,6 +4,8 @@
     {
         public int Id { get; set; }
 
+        public int QuestionId { get; set; }
+
         public string Description { get; set; }
 
         public int EmployeeId { get; set; }
