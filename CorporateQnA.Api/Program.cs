085046ff081f34aa2971dad91b9eb73dfb699564
Komarapu-Raju <raju.k@keka.com>
2023-03-28 22:42:12 +0530

    user 4

--- CorporateQnA.Api/Program.cs
+++ CorporateQnA.Api/Program.cs
@@ -21,8 +21,6 @@ builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();
 
-//user4
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
