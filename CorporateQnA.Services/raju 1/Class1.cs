b2353fdbf13941ef01efd83eba823addaac7c341
Komarapu-Raju <raju.k@keka.com>
2023-04-26 10:47:45 +0530

    commit 1

---
+++ CorporateQnA.Services/raju 1/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorporateQnA.Services.raju_1
+{
+    internal class Class1
+    {
+    }
+}
